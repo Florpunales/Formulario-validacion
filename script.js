54f952103866b7cbd62f3535de53452ef6cea3cd
florenciapunales@gmail.com <florenciapunales@gmail.com>
2025-07-15 18:49:06 -0300

    Modifique script.js con validaciones de formulario

--- script.js
+++ script.js
@@ -9,9 +9,23 @@ function validarFormulario(){
     let validacionCorrecta = true;
 
     camposTexto.forEach(campo => {
-        let errorCampo = document.getElementById('error' + campo.id.slice(1))
-        if(campo.ariaValueMax.length == ''){
-
+        let errorCampo = document.getElementById('error' + campo.id.charAt(0).toUpperCase() + campo.id.slice(1))
+        if(campo.Value.length == ''){
+            mostrarError(errorCampo, 'Este campo es requerido!' )
+            validacionCorrecta = false
+        }else if(campo.value.length > 0 && campo.value.length < 3){
+            ocultarError()
         }
     })
+}
+
+const mostrarError = (elemento, mensaje) => {
+    elemento.textContent = mensaje;
+    elemento.style.display = "block";
+}
+
+const ocultarError = (elemento, mensaje) => {
+    elemento.textContent = '';
+    elemento.style.display = "none";
+
 }
